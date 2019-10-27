d974b4fe6e9e2ee7d15678b51e3a6da3837d3c92
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2019-10-27 18:25:33 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -3,10 +3,21 @@ import { actors } from '../data';
 
 const Actors = () => {
   return (
-    <div>
-      {/*{code here}*/}
+    <div className="actor">
+      <h1>Actors Page</h1>
+      {actors.map(actor => (
+        <div key={actor}>Name: {actor.name}
+           <p>Movies:
+             <ul>
+               {actor.movies.map(movie => (
+                 <li key={movie}>{movie}</li>
+               ))}
+               </ul>
+            </p>
+        </div>
+      ))}
     </div>
   );
-};
+}
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -3,8 +3,20 @@ import { movies } from '../data';
 
 const Movies = () => {
   return (
-    <div>
-        {/*{code here}*/}
+    <div className="movies">
+      <h1>Movies Page</h1>
+        {movies.map(movie => (
+          <div key={movie}>Name: {movie.title}
+            <p>Time:{movie.time}</p>
+            <p>Genres:
+              <ul>
+                {movie.genres.map(genre => (
+                  <li key={genre}>{genre}</li>
+                ))}
+              </ul>
+            </p>
+          </div>
+        ))}
     </div>
   );
 };
